73c69e595cddb3825c8eebb63ea1921d05446a27
Xeon <654452591@qq.com>
2020-06-13 05:33:22 +0800

    update MainActivity.

---
+++ app/src/main/java/com/aone/recorder/AudioRecoder.java
@@ -0,0 +1,15 @@
+package com.aone.recorder;
+
+/**
+ * @ProjectName: Recorder
+ * @Package: com.aone.recorder
+ * @ClassName: AudioRecoder
+ * @Author: Xeon
+ * @University: Gansu Agricultural University
+ * @Department: Information Science and Technology
+ * @Major: Computer Science and Technology
+ * @Date: 2020/6/13 5:26
+ * @Desc:
+ */
+public class AudioRecoder {
+}
--- app/src/main/java/com/aone/recorder/MainActivity.java
+++ app/src/main/java/com/aone/recorder/MainActivity.java
@@ -1,13 +1,57 @@
 package com.aone.recorder;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aone.recorder.DAO.ConfigDAO;
+import com.aone.recorder.model.RecordConfig;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    // 权限
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    // 录音相关
+    private AudioRecoder mAudioRecorder;
+    private ConfigDAO configDAO;
+    private Cursor ConfigCursor;
+    private RecordConfig mRecordConfig;
+
+    private boolean RecordState = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        // 初始化数据库
+        configDAO = new ConfigDAO();
+        configDAO.initConfig(this);
+        // 获取配置数据
+        ConfigCursor = configDAO.queryConfig(this);
+
+
+    }
+
+    /**
+     * 请求权限
+     */
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted) finish();
     }
 }
