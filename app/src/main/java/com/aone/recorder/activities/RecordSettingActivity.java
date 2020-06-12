e1746949b16a3243ef5e36bb04ce3638843d61d3
Xeon <654452591@qq.com>
2020-06-13 04:27:12 +0800

    update activity_record_list.xml.

---
+++ app/src/main/java/com/aone/recorder/activities/RecordSettingActivity.java
@@ -0,0 +1,15 @@
+package com.aone.recorder.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aone.recorder.R;
+
+public class RecordSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_setting);
+    }
+}
