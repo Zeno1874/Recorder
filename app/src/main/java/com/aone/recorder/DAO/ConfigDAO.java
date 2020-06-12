b52a231ae47c82c98fff9b80acee540e991e1300
Xeon <654452591@qq.com>
2020-06-13 03:53:15 +0800

    update DAO.

---
+++ app/src/main/java/com/aone/recorder/DAO/ConfigDAO.java
@@ -0,0 +1,71 @@
+package com.aone.recorder.DAO;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.media.MediaRecorder;
+
+/**
+ * @ProjectName: Recorder
+ * @Package: com.aone.recorder.DAO
+ * @ClassName: ConfigDAO
+ * @Author: Xeon
+ * @University: Gansu Agricultural University
+ * @Department: Information Science and Technology
+ * @Major: Computer Science and Technology
+ * @Date: 2020/6/13 3:42
+ * @Desc: 配置数据表相关操作，该表仅能修改与查询，有且仅有一行数据，只允许在第一次运行时写入。
+ */
+public class ConfigDAO {
+    public static final String CONFIG_TABLE_NAME = "config";
+
+    /**
+     * 初始化配置数据表
+     * @param context
+     */
+    public void initConfig(Context context){
+        DBHelper dbHelper = new DBHelper(context,"recorder",null,1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("AudioSource", MediaRecorder.AudioSource.DEFAULT);
+        values.put("AudioSamplingRate", 44100);
+        values.put("OutputFormat", MediaRecorder.OutputFormat.DEFAULT);
+        values.put("AudioChannels", 2);
+        values.put("AudioEncoder", MediaRecorder.AudioEncoder.DEFAULT);
+        values.put("AudioEncodingBitRate", 192000);
+        values.put("OutputFileFormat", "MP3");
+
+        db.insert(CONFIG_TABLE_NAME,null,values);
+    }
+
+    /**
+     * 更新配置数据表
+     * @param context
+     * @param key 键
+     * @param value 值
+     */
+    public void updateConfig(Context context, String key, String value){
+        DBHelper dbHelper = new DBHelper(context,"recorder",null,1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(key,value);
+
+        db.update(CONFIG_TABLE_NAME,values,"id = ?",new String[]{"1"});
+    }
+
+    /**
+     * 获取配置数据表数据集
+     * @param context
+     * @return Cursor数据集
+     */
+    public Cursor queryConfig(Context context){
+        DBHelper dbHelper = new DBHelper(context,"recorder",null,1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Cursor cursor = db.query(CONFIG_TABLE_NAME, new String[]{"AudioSource", "AudioSamplingRate", "OutputFormat", "AudioChannels", "AudioEncoder", "AudioEncodingBitRate", "OutputFileFormat"}, null, null, null, null, null);
+        cursor.moveToNext();
+        return cursor;
+    }
+}
---
+++ app/src/main/java/com/aone/recorder/DAO/DBHelper.java
@@ -0,0 +1,40 @@
+package com.aone.recorder.DAO;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+/**
+ * @ProjectName: Recorder
+ * @Package: com.aone.recorder.DAO
+ * @ClassName: DBHelper
+ * @Author: Xeon
+ * @University: Gansu Agricultural University
+ * @Department: Information Science and Technology
+ * @Major: Computer Science and Technology
+ * @Date: 2020/6/13 3:36
+ * @Desc:
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String CONFIG_TABLE_NAME = "RecordConfig";
+    public static final String LIST_TABLE_NAME = "RecordList";
+
+    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create_config_db_sql = "create table if not exists " + CONFIG_TABLE_NAME +" (id integer primary key, AudioSource integer, AudioSamplingRate integer, OutputFormat integer, AudioChannels integer, AudioEncoder integer, AudioEncodingBitRate integer, DefaultFilePath text, OutputFileFormat text)";
+        String create_list_db_sql = "create table if not exists " + LIST_TABLE_NAME +" (id integer primary key, FileName text, FileFormat text, FileRecordLength int, FileCreatedTime int)";
+        db.execSQL(create_config_db_sql);
+        db.execSQL(create_list_db_sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
